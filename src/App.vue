90141390fa8c26003de626b3e9a007fd64575528
Piotr Drożdż <piotr.drozdz0@op.pl>
2020-03-25 23:21:08 +0100

    feat: add variables

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
-  <SpotifyProvider>
-    <div id="app">
-      <img alt="Vue logo" src="./assets/logo.png">
-      <HelloWorld />
-    </div>
-  </SpotifyProvider>
+  <div id="app">
+    <SpotifyProvider>
+        <img alt="Vue logo" src="./assets/logo.png">
+        <HelloWorld />
+    </SpotifyProvider>
+  </div>
 </template>
 <script lang="ts" src="./App.ts"></script>
-<style scoped lang="scss" src="./App.scss"></style>
+<style lang="scss" src="./App.scss"></style>
