243ab535eafcb09b6ecba39785fab922d8127f70
Piotr Drożdż <piotr.drozdz0@op.pl>
2020-03-28 01:41:06 +0100

    fix: emit

--- src/components/TopBar/TopBar.ts
+++ src/components/TopBar/TopBar.ts
@@ -33,7 +33,7 @@ export default class SpotifyProvider extends Vue {
     }
   }
 
-  @Emit('artist')
+  @Emit('onArtistChange')
   onArtistChange(value: SpotifyApi.ArtistObjectFull) {
     return value;
   }
