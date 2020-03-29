a52093dafed085435a7d30ff982939ec720e6feb
Piotr Drożdż <piotr.drozdz0@op.pl>
2020-03-29 21:15:44 +0200

    feat: add avatar and name

--- src/components/Artist/Artist.ts
+++ src/components/Artist/Artist.ts
@@ -1,6 +1,10 @@
 import { Vue, Component, Prop } from 'vue-property-decorator';
+import _get from 'lodash/get';
 
 @Component
 export default class SpotifyProvider extends Vue {
   @Prop() public artist!: SpotifyApi.ArtistObjectFull;
+
+  get imageSrc(): string { return _get(this.artist, 'images[0].url'); }
+  get name(): string { return this.artist.name; }
 }
--- src/components/Artist/Artist.vue
+++ src/components/Artist/Artist.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <h1>Hello {{artist.name}}!</h1>
+    <v-avatar size="160" v-if="imageSrc">
+      <img :src="imageSrc">
+    </v-avatar>
+    <h2 class="name">{{name}}</h2>
   </div>
 </template>
 <script src="./Artist.ts" lang="ts"></script>
+<style scoped lang="scss" src="./Artist.scss"></style>
