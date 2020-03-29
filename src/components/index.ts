31a45c036b72bc2d7f45c8a528be9f348c621b68
Piotr Drożdż <piotr.drozdz0@op.pl>
2020-03-29 23:38:06 +0200

    fix: remove unnecessary eslint-disable

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,3 @@
-/* eslint-disable quote-props */
 import Artist from '@/components/Artist/Artist.vue';
 import SpotifyProvider from '@/components/SpotifyProvider/SpotifyProvider.vue';
 import TopBar from '@/components/TopBar/TopBar.vue';
