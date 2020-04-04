574cc9b4790c3bb2eaf3658e3cd6d13858e584c0
Adrian Kubala <adr.kubala@gmail.com>
2020-04-04 19:19:53 +0200

    Add basic scss styles

--- src/components/Welcome/Welcome.vue
+++ src/components/Welcome/Welcome.vue
@@ -2,8 +2,10 @@
   <div>
       <div class="header">
       <h1>Know your music!</h1>
-      <h4>Have you ever wanted to automatically listen to your favourite music and at the same time find useful info about the author?</h4>
+      <br>
+      <h3>Have you ever wanted to automatically listen to your favourite music and at the same time find useful info about the author?</h3>
       <h4>We neither, but now you can do this!</h4>
     </div>
   </div>
 </template>
+<style scoped lang="scss" src="./Welcome.scss"></style>
