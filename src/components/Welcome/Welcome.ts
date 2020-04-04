82a8de52213cbb71de4508052627b73fa5051e74
Krzysztof Koczwara <krzykocz@gmail.com>
2020-04-05 00:43:12 +0200

    Show top artists

--- src/components/Welcome/Welcome.ts
+++ src/components/Welcome/Welcome.ts
@@ -3,16 +3,24 @@ import SpotifyWebApi from "spotify-web-api-js";
 import { SPOTIFY_API_PROVIDER } from "@/consts/providers";
 import spotifyApiCatch from "@/utils/spotifyApiCatch";
 import _map from "lodash/map";
+import _slice from"lodash/slice";
+
 
 @Component
 export default class SpotifyProvider extends Vue {
   @Inject(SPOTIFY_API_PROVIDER) public spotifyApi!: SpotifyWebApi.SpotifyWebApiJs;
-  public topArtists = this.getTopArtists();
-  private getTopArtists() {
-    this.spotifyApi.getMyTopArtists({ limit: 6 }).then((data) => {
-        console.log("test")
-        console.log(data.items);
-        return data.items;
-    });
+  public topArtists: Array<SpotifyApi.ArtistObjectFull> = [];
+
+  constructor() {
+      super();
+      this.getTopArtists();
+  }
+  private getTopArtists(): void {
+    this.spotifyApi.getMyTopArtists({ limit: 6 }).then(
+        ({items}: SpotifyApi.UsersTopArtistsResponse) => {
+            this.topArtists = items;
+            console.log(this.topArtists)
+        }
+    ).catch(spotifyApiCatch);
   }
 }
