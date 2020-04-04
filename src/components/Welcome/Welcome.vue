68dece2a06db39724642198bc7f22a7b1eeb2375
Adrian Kubala <adr.kubala@gmail.com>
2020-04-04 19:27:09 +0200

    First attempt to paste logo

--- src/components/Welcome/Welcome.vue
+++ src/components/Welcome/Welcome.vue
@@ -3,8 +3,9 @@
       <div class="header">
       <h1>Know your music!</h1>
       <br>
-      <h3>Have you ever wanted to automatically listen to your favourite music and at the same time find useful info about the author?</h3>
-      <h4>We neither, but now you can do this!</h4>
+      <img src="@/assets/logo-black-full.png" alt="WikiFy main logo">
+      <h4>Have you ever wanted to automatically listen to your favourite music and at the same time find useful info about the author?</h4>
+      <h3>We neither, but now you can do this!</h3>
     </div>
   </div>
 </template>
