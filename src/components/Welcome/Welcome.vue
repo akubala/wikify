99460cca2c1201058a701113a82ed7a73c43a484
Adrian Kubala <adr.kubala@gmail.com>
2020-04-05 12:35:04 +0200

    Fix unresponsive containers

--- src/components/Welcome/Welcome.vue
+++ src/components/Welcome/Welcome.vue
@@ -1,21 +1,35 @@
 <template>
-  <div>
-    <div class="header">
-      <h1>Know your music!</h1>
-      <br />
-      <img src="@/assets/logo-transparent-full.png" alt="WikiFy main logo" />
-      <h4>Have you ever wanted to automatically listen to your favourite music and at the same time find useful info about the author?</h4>
-      <h3>We neither, but now you can do this!</h3>
-    </div>
-    <v-divider />
-    <div class="top-artists">
-      <div class="artist">
-        <v-avatar size="100" :title="artist.name" v-for="artist in topArtists" v-bind:key="artist.name">
-          <img :src="artist.images[0].url" />
-        </v-avatar>
+  <v-container fluid>
+    <v-row justify="space-around">
+      <div>
+        <div class="header">
+          <h1>Know your music!</h1>
+          <br />
+          <v-img
+            :src="require('@/assets/logo-transparent-full.png')"
+            max-height="300"
+            max-width="800"
+            alt="WikiFy main logo"
+          ></v-img>
+          <h4>Have you ever wanted to automatically listen to your favourite music and at the same time find useful info about the author?</h4>
+          <h3>We neither, but now you can do this!</h3>
+        </div>
+        <v-divider />
+        <div class="top-artists">
+          <div class="artist">
+            <v-avatar
+              size="100"
+              :title="artist.name"
+              v-for="artist in topArtists"
+              v-bind:key="artist.name"
+            >
+              <img :src="artist.images[0].url" />
+            </v-avatar>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
+    </v-row>
+  </v-container>
 </template>
 <script src="./Welcome.ts" lang="ts"></script>
 <style scoped lang="scss" src="./Welcome.scss"></style>
